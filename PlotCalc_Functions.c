bf7e886a92413cc7debb48a1473ec0714cb66885
Dalone <nikita.odinokih@gmail.com>
2014-05-19 22:13:45 +0700

    Plot revert

--- PlotCalc_Functions.c
+++ PlotCalc_Functions.c
@@ -19,21 +19,18 @@ Note* Pointer_vars;
 List* str_to_inf(char* in)
 {
 	/*Used inside string_analyse*/
-	return NULL;
 }
 
 //Queue to postfix
 List* inf_to_post(List* inf)
 {
 	/*Used inside string_analyse*/
-	return NULL;
 }
 
 //Calculate postfix
 double post_calc(List* post)
 {
 	/*Used inside string_analyse by ariph and plots (in cycle)*/
-	return 0;
 }
 
 //Transferes answer to the rational form
@@ -46,20 +43,18 @@ Rat_num ans_to_rat(double ans)
 int session_open()
 {
 	/*opens files*/
-	return 0;
 }
 
 //Save plot
 int plot_save(Note* point)
 {
-	return 0;
+
 }
 
 //Close session of storage
 int session_close(List* ariph, List* var)
 {
 	/*saves the rest*/
-	return 0;
 }
 
 //Variables list
@@ -71,5 +66,5 @@ void interface_list_vars(List* var)
 //Analyse string and calculate/save/build
 int string_analyse(char* str, Note* point, List *ariph)
 {
-	return 0;
+
 }
